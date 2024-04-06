2baa15540808036955d6d33d0e6be9d7ca3cb3d1
namishkashyap <36044596+namishkashyap@users.noreply.github.com>
2024-04-07 00:41:02 +0530

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -5,19 +5,19 @@ import cookieParser from "cookie-parser"
 
 
 const app = express()
-// app.use(cors({
-//     origin: ["https://flixxit-frontend-beta.vercel.app"],
-//     methods: ["POST", "GET", "PUT"],
-//     credentials: true
-// }))
+app.use(cors({
+    origin: ["https://flixxit-frontend-beta.vercel.app"],
+    methods: ["POST", "GET", "PUT"],
+    credentials: true
+}))
 
-app.use(function (req, res, next) {
-    //Enabling CORS
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
-    });
+// app.use(function (req, res, next) {
+//     //Enabling CORS
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Credentials", "true");
+//     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+//     res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
+//     });
 
 app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
