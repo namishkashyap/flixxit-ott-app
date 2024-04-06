6170e52d493f6a6943725ef831b255b569c65159
namishkashyap <36044596+namishkashyap@users.noreply.github.com>
2024-04-07 00:24:14 +0530

    Update constants.jsx

--- flixxit/src/utils/constants.jsx
+++ flixxit/src/utils/constants.jsx
@@ -1,4 +1,4 @@
 export const API_KEY = "b076b8d96414615b3b12df1430ed4bc4";
 export const TMBD_BASE_URL = "https://api.themoviedb.org/3";
 export const DB_NAME = "flexxitDB";
-export const API_END_POINT = "https://flixxit-server-3197.onrender.com/user";
+export const API_END_POINT = "https://flixxit-server-3197.onrender.com";
