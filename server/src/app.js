06128a1ea9597661cc402a05ba19e480d067b599
namishkashyap <36044596+namishkashyap@users.noreply.github.com>
2024-04-06 23:52:55 +0530

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -14,9 +14,9 @@ const app = express()
 app.use(function (req, res, next) {
     //Enabling CORS
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Credentials" : "true");
-    res.header("Access-Control-Allow-Methods" : "GET, POST, OPTIONS");
-    res.header("Access-Control-Allow-Headers" : "Origin, Content-Type, Accept");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
     });
 
 app.use(express.json({ limit: "16kb" }))
