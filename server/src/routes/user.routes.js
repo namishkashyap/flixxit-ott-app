a7f9ce3bfd900cf0e6302b2345f0e0078d4c34ef
Wollvo-Design <wollvodesign@gmail.com>
2024-04-08 19:23:36 +0530

    Removed updateUser

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { addMovieLista, getMovieLiked, login, logout, registerUser, removeMovieFromList, updatedUser } from "../controlers/user.controller.js";
+import { addMovieLista, getMovieLiked, login, logout, registerUser, removeMovieFromList } from "../controlers/user.controller.js";
 
 
 const router = Router()
@@ -8,7 +8,7 @@ const router = Router()
 router.route("/signup").post(registerUser)
 router.route("/login").post(login)
 router.route("/logout").get(logout)
-router.route("/updateuser").put(updatedUser)
+// router.route("/updateuser").put(updatedUser)
 router.route("/add").post(addMovieLista)
 router.route("/liked/:email").get(getMovieLiked)
 router.route("/delete").put(removeMovieFromList)
